5dbe2ce394e219bca524698166ca474876111bbf
ivansolobear <ivansolobear@gmail.com>
2019-04-16 17:21:45 +0300

    FIX: moved react app one folder upwards.

--- src/index.js
+++ src/index.js
@@ -29,4 +29,6 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//FIXME: if something breaks comment this back in
+// serviceWorker.unregister();
