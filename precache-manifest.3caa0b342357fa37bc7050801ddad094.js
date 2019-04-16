764c1e847483b1af6670254a092f8c0c38457b5a
ivansolobear <ivansolobear@gmail.com>
2019-04-16 15:15:43 +0300

    Updates

---
+++ precache-manifest.3caa0b342357fa37bc7050801ddad094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "362ff82adf0bfeebf5e807eb084e4bb2",
+    "url": "/Movies-app/static/media/bg.362ff82a.png"
+  },
+  {
+    "revision": "b702313c276edfd7d1a4",
+    "url": "/Movies-app/static/js/runtime~main.ee86f040.js"
+  },
+  {
+    "revision": "e2abeb7ac96f455af16e",
+    "url": "/Movies-app/static/js/main.252e4b2d.chunk.js"
+  },
+  {
+    "revision": "ab0a2fa7ad7f53be9096",
+    "url": "/Movies-app/static/js/2.ece8e83d.chunk.js"
+  },
+  {
+    "revision": "e2abeb7ac96f455af16e",
+    "url": "/Movies-app/static/css/main.eebb55da.chunk.css"
+  },
+  {
+    "revision": "a9a720e546f84cdd7bbc3a085261addb",
+    "url": "/Movies-app/index.html"
+  }
+];
